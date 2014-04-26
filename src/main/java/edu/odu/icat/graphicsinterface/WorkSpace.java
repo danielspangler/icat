984327af44938e8438e6fcfee4a1ad027bf6e0b5
abinx001 <abinx001@odu.edu>
2014-04-26 13:46:47 -0400

    This is just a test for the files filter... DON'T TOUCH IT

--- src/main/java/edu/odu/icat/graphicsinterface/WorkSpace.java
+++ src/main/java/edu/odu/icat/graphicsinterface/WorkSpace.java
@@ -7,11 +7,11 @@ package edu.odu.icat.graphicsinterface;
 import com.mxgraph.model.mxCell;
 import edu.odu.icat.analytics.AnalyticsEngine;
 import edu.odu.icat.controller.Control;
+import edu.odu.icat.controller.FileExtensions;
 import edu.odu.icat.graphicsinterface.editor.EditorActions;
 import edu.odu.icat.model.Entity;
 import edu.odu.icat.model.Force;
 import edu.odu.icat.model.Project;
-import edu.odu.icat.service.ProjectDAO;
 
 import javax.swing.*;
 import javax.swing.filechooser.FileFilter;
@@ -459,7 +459,7 @@ public class WorkSpace extends JFrame {
     class SaveAsAction implements ActionListener {
         JFileChooser fc = new JFileChooser();
         FileFilter filter = new FileNameExtensionFilter("ICAT Files", "icat");
-
+        FileExtensions fileExtensions;
         public void actionPerformed(ActionEvent e)
         {
             // JOptionPane.showMessageDialog(WorkSpace.this, "No Files Found.");
